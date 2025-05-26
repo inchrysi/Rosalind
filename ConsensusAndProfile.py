097ff4923a32e8772b7edaa607f7c2cba9c9cfee
inchrysi <chriselleannguillermo@gmail.com>
2025-05-26 20:59:10 +0200

    Figuring out matrixes

--- ConsensusAndProfile.py
+++ ConsensusAndProfile.py
@@ -9,4 +9,50 @@ def readFASTA(txtfile):
                 chiave = f[1:]
                 nomieseq[chiave] = ''
             else:
-                nomieseq[chiave] 
+                nomieseq[chiave]+= f
+    
+    return nomieseq
+
+def consAndProf(txtfile):
+    collection = readFASTA(txtfile)
+    
+    seqs = []
+    
+    
+    for key in collection:
+        item = collection.get(key)
+        seqs.append(item)
+    
+    apparition = []
+    
+    for x in seqs:
+        A = []
+        C = []
+        G = []
+        T = []
+        
+        for i in x:
+            if i == 'A':
+                A.append(1)
+            if i == 'C':
+                C.append(1)
+            if i == 'G':
+                G.append(1)
+            if i == 'T':
+                T.append(1)
+        apparition.append(A)
+        apparition.append(C)
+        apparition.append(G)
+        apparition.append(T)
+            
+    return apparition
+'''
+            if i == 'A':
+                A+= 1
+            if i == 'C':
+                C+= 1
+            if i == 'G':
+                G+= 1
+            if i == 'T':
+                T+= 1
+'''
