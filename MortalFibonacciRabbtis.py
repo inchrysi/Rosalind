0ae6228d5adadeba235452415115a51713ff42b9
inchrysi <chriselleannguillermo@gmail.com>
2025-06-08 14:09:09 +0200

    mortal fibonacci rabbits

--- MortalFibonacciRabbtis.py
+++ MortalFibonacciRabbtis.py
@@ -1,10 +1,15 @@
-def rabbitsRecurrence(n, k, m):
+def mortalRabbits(n, k, m):
     fibo = [0, 1, 1]
+    monthCounter = 0
     
     for i in range(2, n):
         a = fibo[i]
         b = k * (fibo [i - 1])
         coso = a + b
+        monthCounter+= 1
+        if monthCounter == m + 1:
+            coso = coso - fibo[i - m]
+            monthCounter = 0
         fibo.append(coso)
         
     print(fibo, fibo[n])
@@ -12,3 +17,5 @@ def rabbitsRecurrence(n, k, m):
 #n -> months
 #k  ->litter of pairs
 #m -> lifetime
+    
+#this already considers pairs
